7446c15c3d821c198900efc52927ec8654b2cb81
rabi sahu <raviraz.sahu15@gmail.com>
2023-07-10 22:50:03 +0530

    adding file to practice git merge feature

---
+++ src/practice1/gitaddingjavafilewithsyso.java
@@ -0,0 +1,10 @@
+package practice1;
+
+public class gitaddingjavafilewithsyso {
+
+	public static void main(String[] args) {
+
+		System.out.println("Git practice branch name is addingonejavafilepractice");
+	}
+
+}
