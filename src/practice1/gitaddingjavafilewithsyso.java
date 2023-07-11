2fa712e3447197e53e0fca63e7292ceb46dcfb94
rabi sahu <raviraz.sahu15@gmail.com>
2023-07-11 08:26:08 +0530

    adding print1() method

--- src/practice1/gitaddingjavafilewithsyso.java
+++ src/practice1/gitaddingjavafilewithsyso.java
@@ -7,8 +7,14 @@ public class gitaddingjavafilewithsyso {
 	public static void main(String[] args) {
 
 		print();
+		print1();
 	}
 	private static void print() {
+		System.out.println("Git practice branch name is addingonejavafilepractice");
+
+	}
+	
+	private static void print1() {
 		System.out.println("Git practice branch name is addingonejavafilepractice");
 
 	}
